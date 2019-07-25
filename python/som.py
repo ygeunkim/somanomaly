230f03882118a05cd3a46af026c009eb399b5b51
young-geun <dudrmshub@gmail.com>
2019-07-25 17:10:54 +0900

    learning som

--- python/som.py
+++ python/som.py
@@ -25,13 +25,13 @@ class kohonen:
         Learning rate: ⍺(t) = ⍺_0 * exp(-t / ƛ)
     """
 
-    def __init__(self, data, xdim, ydim, topo = "rectangular", neighbor, dist = "frobenius"):
+    def __init__(self, data, xdim, ydim, topo = "rectangular", neighbor = "gaussian", dist = "frobenius"):
         """
         :param data: 3d array. processed data set for Online SOM Detector
         :param xdim: Number of x-grid
         :param ydim: Number of y-grid
         :param topo: Topology of output space - rectangular or hexagonal
-        :param neighbor: Neighborhood function - gaussian or
+        :param neighbor: Neighborhood function - gaussian or bubble
         :param dist: Distance function - frobenius or
         """
         self.net_dim = np.array([xdim, ydim])
@@ -45,8 +45,9 @@ class kohonen:
             raise ValueError("Invalid topo. Expected one of: %s" % topo_types)
         self.topo = topo
         self.init_grid()
+        self.dist_node()
         # Neighborhood function
-        neighbor_types = ["gauss"]
+        neighbor_types = ["gaussian", "bubble"]
         if neighbor not in neighbor_types:
             raise ValueError("Invalid neighbor. Expected one of: %s" % neighbor_types)
         self.neighbor_func = neighbor
@@ -55,6 +56,12 @@ class kohonen:
         if dist not in dist_type:
             raise ValueError("Invalid dist. Expected one of: %s" % dist_type)
         self.dist_func = dist
+        # som()
+        self.alpha = None
+        self.sigma = None
+        self.time_constant = None
+        # find_bmu()
+        self.bmu = None
 
     def init_weight(self):
         self.net = np.random.rand(self.net_dim[0] + self.net_dim[1], self.ncol, self.nrow)
@@ -70,15 +77,24 @@ class kohonen:
             self.pts[:, 0] = self.pts[:, 0] + .5 * (self.pts[:, 0] % 2)
             self.pts[:, 1] = np.sqrt(3) / 2 * self.pts[:, 1]
 
-    def som(self, data, epoch, init_rate, init_radius):
+    def som(self, data, epoch = 100, init_rate = None, init_radius = None):
         """
         :param data: 3d array. processed data set for Online SOM Detector
         :param epoch: epoch number
         :param init_rate: initial learning rate
+        :param init_radius: initial radius of BMU neighborhood
         :return:
         """
         num_obs = data.shape[0]
-        self.alpha = init_rate
+        obs_id = np.arange(num_obs)
+        chose_i = None
+        node_id = None
+        hci = None
+        # learning rate
+        if init_rate is None:
+            self.alpha = .05
+        else:
+            self.alpha = init_rate
         # radius of neighborhood
         if init_radius is None:
             self.sigma = np.quantile(self.dci, 2 / 3)
@@ -86,14 +102,24 @@ class kohonen:
             self.sigma = init_radius
         # time constant (lambda)
         self.time_constant = epoch / np.log(self.sigma)
-        obs_id = np.arange(num_obs)
+        # BMU pair
         self.find_bmu(data)
+        bmu_dist = self.dci[1, :]
         for i in range(1, epoch + 1):
+            chose_i = np.random.choice(obs_id)
+            # BMU
+            bmu_node = self.bmu[chose_i - 1]
+            bmu_dist = self.dci[bmu_node, :]
             # decay
-            self.sigma = self.decay(init_radius, i, self.time_constant)
-            self.alpha = self.decay(init_rate, i, self.time_constant)
-            for k in range(1, self.net_dim[0] * self.net_dim[1] + 1):
+            self.sigma = kohonen.decay(init_radius, i, self.time_constant)
+            self.alpha = kohonen.decay(init_rate, i, self.time_constant)
+            # neighboring nodes
+            neighbor_neuron = np.where(bmu_dist <= self.sigma)
+            for k in range(1, neighbor_neuron.shape[0] + 1):
+                node_id = neighbor_neuron[k - 1]
+                hci = self.neighborhood(bmu_dist[node_id], self.sigma)
                 # update codebook
+                self.net[node_id, :, :] += self.alpha * hci * (data[chose_i - 1, :, :] - self.net[node_id, :, :])
 
     def find_bmu(self, data):
         """
@@ -111,6 +137,7 @@ class kohonen:
         """
         :param data: Processed data set for SOM.
         :param index: Randomly chosen observation id for input matrix among 3d tensor set.
+        :param node: node index
         :return: distance between input matrix observation and weight matrix of the node
         """
         if self.dist_func == "frobenius":
@@ -127,20 +154,29 @@ class kohonen:
         else:
             self.dci = distance.cdist(self.pts[:, 0], self.pts[:, 1], "chebyshev")
 
-    def decay(self, init, time, time_constant):
+    @staticmethod
+    def decay(init, time, time_constant):
+        """
+        :param init: initial value
+        :param time: t
+        :param time_constant: lambda
+        :return: decaying value of alpha or sigma
+        """
         return init * np.exp(-time / time_constant)
 
-    @staticmethod
-    def neighborhood(distance, radius):
+    def neighborhood(self, distance, radius):
         """
         :param distance: Distance between SOM neurons
         :param radius: Radius of BMU neighborhood
         :return: Neighborhood function hci
         """
-        if self.neighbor_func == "gauss":
+        if self.neighbor_func == "gaussian":
             return np.exp(-distance ** 2 / (2 * (radius ** 2)))
         else:
-            return np.exp(-distance ** 2 / (2 * (radius ** 2)))
+            if distance <= radius:
+                return 1.0
+            else:
+                return 0.0
 
 
 
